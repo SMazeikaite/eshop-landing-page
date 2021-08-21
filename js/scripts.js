24b4358ad4a7893fa1badd5c55be08f794f92728
SMazeikaite <samantamazz@gmail.com>
2021-08-21 12:30:33 +0300

    Changed styling; Added title on top of image"

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
-function previousSlide() {
-    const currCard = document.querySelector(".slide.visible");
+'use strict'
+
+const previousSlideBtn = document.getElementsByClassName('previous-slide')[0];
+const nextSlideBtn = document.getElementsByClassName('next-slide')[0];
+let currCard = document.querySelector('.slide.visible');
+
+previousSlideBtn.addEventListener('click', function() {
+    currCard = document.querySelector('.slide.visible');
     const prevCard = currCard.previousElementSibling ?
         currCard.previousElementSibling :
-        document.querySelector(".slides-container").lastElementChild;
-    currCard.classList.remove("visible");
-    prevCard.classList.add("visible");
-}
+        document.querySelector('.slides-container').lastElementChild;
+    currCard.classList.remove('visible');
+    prevCard.classList.add('visible');
+});
 
-function nextSlide() {
-    const currCard = document.querySelector(".slide.visible");
+nextSlideBtn.addEventListener('click', function() {
+    currCard = document.querySelector('.slide.visible');
     const nextCard = currCard.nextElementSibling ?
         currCard.nextElementSibling :
-        document.querySelector(".slides-container").firstElementChild;
-    currCard.classList.remove("visible");
-    nextCard.classList.add("visible");
-}
+        document.querySelector('.slides-container').firstElementChild;
+    currCard.classList.remove('visible');
+    nextCard.classList.add('visible');
+});
