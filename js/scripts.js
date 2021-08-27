c4403671536607f9514155ecaa51061239d32c99
Samanta Mazeikaite <samanta.mazeikaite@visma.com>
2021-08-27 14:06:32 +0300

    use json-server to load carousel items

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
 'use strict'
 
+const container = document.querySelector('.slides-container');
+
+const renderSlides = async () => {
+    let uri = 'http://localhost:3000/slides'
+
+    const slides = await fetch(uri).then(res => res.json());
+    
+    let template = '';
+    slides.forEach(slide => {
+        template += `
+            <div class="${slide.id === 1 ? 'slide visible' : 'slide'}">
+                <img src="${slide.imageUrl}" style="width:100%" alt="slide${slide.id}" />
+                <a href="#" class="slide-title">
+                    <span class="main-color">${slide.productType}</span>
+                    <span class="white-10-color">|</span> ${slide.title}
+                </a>
+            </div>
+        `
+    });
+
+    container.innerHTML = template;
+}
+
+// don't want to pass event object so using arrow function (15:50 tut)
+window.addEventListener('DOMContentLoaded', () => renderSlides());
+
 const previousSlideBtn = document.getElementsByClassName('previous-slide')[0];
 const nextSlideBtn = document.getElementsByClassName('next-slide')[0];
 let currCard = document.querySelector('.slide.visible');
